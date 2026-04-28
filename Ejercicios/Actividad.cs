f590f5efb234f4a651b9dae741d6719adbbf6295
IgnacioEmanuelIencinella <iei96@hotmail.com>
2026-04-28 07:35:32 -0300

    Resolucion de la actividad 5

--- Ejercicios/Actividad.cs
+++ Ejercicios/Actividad.cs
@@ -3,15 +3,19 @@
 
 namespace Ejercicios
 {
-    class ActividadUno
+    class ActividadC5
     {
+        int Vu , n = 0;
+        //Actividad 1
         public int Suma(int A, int B) {
             return A + B;
         }
-
+        //Actividad 3
         public double Promedio(int Tot, int Opcion) {
             return Tot / (Opcion-1);
         }
-
+        public int Resto(int A, int B) {
+           return A % B;
+        }
     }
 }
--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -12,17 +12,19 @@ namespace Ejercicios
         static void Main(string[] args)
         {
             int A, B, Opcion, Tot = 0;
-            ActividadUno A1 = new ActividadUno();
+            ActividadC5 A1 = new ActividadC5();
 
             Console.WriteLine("Seleccione a que actividad quiere entrar");
             Console.WriteLine("Precione 1 para la actividad 1 (suma de dos Números)");
             Console.WriteLine("Precione 3 para la actividad 3 (Promedio de 10 Números)");
+            Console.WriteLine("Precione 5 para la actividad 5 (Número Primos)");
 
             Opcion = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($"Bienvenido a la actividad {Opcion}");
+            //Uso de formato swich para invocar las actividades, esto reduce el uso de memoria
             switch (Opcion) {
                 case 1:
-                    Console.Clear();
-                    Console.WriteLine("Bienvenido a la actividad 1");
                     Console.WriteLine("Escriba el Primer número");
                     A = Convert.ToInt16(Console.ReadLine());
                     Console.WriteLine("Escriba el Segundo número");
@@ -30,8 +32,6 @@ namespace Ejercicios
                     Console.WriteLine($"La suma de {A} y de {B} es: " + A1.Suma(A, B));
                     break;
                 case 3:
-                    Console.Clear();
-                    Console.WriteLine("Bienvenido a la actividad 3");
                     for (Opcion = 1; Opcion <= 10; Opcion++)
                     {
                         Console.WriteLine($"Por Favor, ingrese el número {Opcion}");
@@ -40,11 +40,26 @@ namespace Ejercicios
                     }
                     Console.WriteLine($"El premedio de los {(Opcion-1)} números es de: " + A1.Promedio(Tot, Opcion));
                     break;
-                default:
-                    Console.WriteLine("Opcion incorrecta");
+                case 5:
+                    Tot = 1;
+                    Console.WriteLine("Escribe un número para saber si es primo o no");
+                    A = Convert.ToInt32(Console.ReadLine());                    
+                    B = 2;
+                    while (Tot != 0){                        
+                        Tot = A1.Resto(A, B);
+                        Console.WriteLine($"Vuelta {B} con resto {Tot}");
+                        B = B + 1;
+                    };
+                    if (B != A){
+                        Console.WriteLine($"El valor de {A} es Primo");
+                    }else{
+                        Console.WriteLine($"El valor de {A} no es Primo");
+                    }
                     break;
-            }
-            
+                default:
+                    Console.WriteLine("Nha, es broma, esta actividad no exite");
+                    break;
+            }            
             Console.WriteLine("Preciona cualquier tecla para finalizar");
             Console.ReadKey();
         }
